fcc44816257f1aaac0da279846f381102f7214ef
vtagliaferro <tagli.vinicius@gmail.com>
2017-09-02 10:14:53 -0400

    config server

---
+++ jokenpo-config-server/src/main/java/br/com/vtagliaferro/jokenpo/jokenposrc/JokenpoSrcApplication.java
@@ -0,0 +1,12 @@
+package br.com.vtagliaferro.jokenpo.jokenposrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokenpoSrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokenpoSrcApplication.class, args);
+	}
+}
